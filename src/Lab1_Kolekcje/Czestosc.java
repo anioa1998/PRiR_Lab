d67897809f8d7947dc20ceef2040775011a6db14
anioa1998 <anioa1998@wp.pl>
2019-10-09 22:36:51 +0200

    Counting works

--- src/Lab1_Kolekcje/Czestosc.java
+++ src/Lab1_Kolekcje/Czestosc.java
@@ -6,14 +6,26 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+import static jdk.nashorn.internal.objects.NativeString.toUpperCase;
+
+
 public class Czestosc {
     public static void main(String[] args) {
 
 
         try (Scanner sc = new Scanner(new File("tekst.txt"))) {
 
-            Map<String, Integer> mp = new LinkedHashMap<>();
+            Map<String, Integer> map = new LinkedHashMap<>();
 
+
+            while (sc.hasNext()) {
+                String word = toUpperCase(sc.next()).replaceAll("[^\\p{L}\\p{N}]+", ""); //formuła na wczytywanie znaków unicode w regex
+                if(map.containsKey(word))
+                    map.put(word, map.get(word) + 1);
+                else
+                    map.put(word, 1);
+            }
+            System.out.println(map);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
