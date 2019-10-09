e446a7149f2bcd2b1d28d155d88f16f58dbe02a2
anioa1998 <anioa1998@wp.pl>
2019-10-09 21:56:09 +0200

    Added Map

--- src/Lab1_Kolekcje/Czestosc.java
+++ src/Lab1_Kolekcje/Czestosc.java
@@ -1,7 +1,21 @@
 package Lab1_Kolekcje;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Czestosc {
     public static void main(String[] args) {
 
+
+        try (Scanner sc = new Scanner(new File("tekst.txt"))) {
+
+            Map<String, Integer> mp = new LinkedHashMap<>();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
