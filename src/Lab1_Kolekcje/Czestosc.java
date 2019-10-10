91cae9a1eaa670819dd9d2ae61866746a0d79883
anioa1998 <anioa1998@wp.pl>
2019-10-10 22:40:15 +0200

    Finished with bugs but finished

--- src/Lab1_Kolekcje/Czestosc.java
+++ src/Lab1_Kolekcje/Czestosc.java
@@ -18,17 +18,32 @@ public class Czestosc {
             Map<String, Integer> map = new TreeMap<>();
 
 
-
             while (sc.hasNext()) {
+
                 String word = toUpperCase(sc.next()).replaceAll("[^\\p{L}\\p{N}]+", ""); //formuła na wczytywanie znaków unicode w regex
-                if(map.containsKey(word))
+                if (map.containsKey(word))
                     map.put(word, map.get(word) + 1);
                 else
                     map.put(word, 1);
             }
-            System.out.println(map);
+            int i = 0;
+
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                if (i != 0 && i < 11) {
+                    String k = entry.getKey();
+                    Integer v = entry.getValue();
+                    System.out.println("Word: " + k + "; How many times: " + v);
+                    i++;
+                } else if (i == 0)
+                    i++;
+                else
+                    break;
+
+            }
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 }
+//17.10 10:00
