00e26e5debee768e7e9d9b33e66ada24c9a43b0b
anioa1998 <anioa1998@wp.pl>
2019-10-14 15:15:22 +0200

    Class Osoba done

--- src/Lab2_Zajecia/ksiazkaTelefoniczna/Osoba.java
+++ src/Lab2_Zajecia/ksiazkaTelefoniczna/Osoba.java
@@ -1,4 +1,46 @@
 package Lab2_Zajecia.ksiazkaTelefoniczna;
 
 public class Osoba {
+    private static int id_licznik = 0;
+
+    private int id;
+    private String imie;
+    private String nazwisko;
+    private String nrTelefonu;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public String getNrTelefonu() {
+        return nrTelefonu;
+    }
+
+    public void setNrTelefonu(String nrTelefonu) {
+        this.nrTelefonu = nrTelefonu;
+    }
+
+    public Osoba(String imie, String nazwisko, String nrTelefonu) {
+        id_licznik += 1;
+        this.id = id_licznik;
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.nrTelefonu = nrTelefonu;
+    }
 }
